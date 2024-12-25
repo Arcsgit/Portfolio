bcce327d851c86e19003a1098e00c6fc1d1b2bc0
Arcsgit <ars.archit6@gmail.com>
2024-12-26 00:42:16 +0530

    First responsive changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, Inject } from '@angular/core';
 import { CommonModule, DOCUMENT } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -13,4 +14,5 @@ export class AppComponent {
   title = 'Portfolio';
   constructor(@Inject(DOCUMENT) private document: Document) { }
   goToLinktree(): void { this.document.location.href = 'https://linktr.ee/archit6'; }
+  hamburgerBtn(): void {}
 }
